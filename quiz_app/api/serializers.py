3828518fbc99c6a4ff52ea0ffae3f3a00721fd64
Getinger96 <erich.getinger@outlook.de>
2025-12-01 18:54:47 +0100

    implemented changes in serializer and view to get the right response

--- quiz_app/api/serializers.py
+++ quiz_app/api/serializers.py
@@ -3,9 +3,21 @@ from ..models import Quiz,Question
 import json
 
 class QuestionSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model=Question
         fields=['id','question_title','question_options','answer','created_at','updated_at']
+
+        def validate(self, data):
+         options = data.get("question_options", [])
+         answer = data.get("answer", "").strip()
+
+         if answer not in options:
+            raise serializers.ValidationError({
+                "answer": f"Answer must be one of {options}"
+            })
+
+         return data
 
 
 class QuizSerializer(serializers.ModelSerializer):
@@ -17,19 +29,28 @@ class QuizSerializer(serializers.ModelSerializer):
     
 
 class QuizCreateSerializer(serializers.ModelSerializer):
-    url=serializers.URLField(required=False,allow_null=True)
-    questions=QuestionSerializer(many=True,required=False)
+    
+    questions=QuestionSerializer(many=True,read_only=True)
 
 
     class Meta:
         model=Quiz
-        fields=['url','questions']
+        fields=['title',
+            'description',
+            'created_at',
+            'updated_at',
+            'video_url',
+            'questions']
 
     def create (self,validated_data):
+        print(validated_data)
         questions_data= validated_data.pop('questions',[])
+        print(validated_data)
+        print(questions_data)
         quiz=Quiz.objects.create(**validated_data)
-        for questions_data in questions_data:
-            Question.objects.create(quiz=quiz,**questions_data)
+        for questions in questions_data:
+            Question.objects.create(quiz=quiz,**questions)
+            print(quiz)
         return quiz
 
 
